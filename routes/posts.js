ca97941d16ee83b74e3b986ec71f303d8cc6984a
Klayton Faria <klayton.souza@gmail.com>
2015-03-03 17:45:10 -0300

    Refactoring posts route

--- routes/posts.js
+++ routes/posts.js
@@ -1,29 +1,9 @@
 module.exports = function(app) {
   var posts = require("../controllers/posts")(app);
 
-  // Get all posts
-  app.get(app.custom.paths.posts.default, function(req, res) {
-    posts.getAll(req, res);
-  });
-
-  // Get single post
-  app.get(app.custom.paths.posts.single, function(req, res) {
-    posts.getOne(req, res);
-  });
-
-  // Remove post
-  app.delete(app.custom.paths.posts.remove, function(req, res) {
-    posts.remove(req, res);
-  });
-
-  // Create post
-  app.post(app.custom.paths.posts.new, function(req, res) {
-    posts.insert(req, res);
-  });
-
-  // Update post
-  app.put(app.custom.paths.posts.update, function(req, res) {
-    posts.update(req, res);
-  });
-
+  app.get(app.custom.paths.posts.default,   posts.getAll)
+     .get(app.custom.paths.posts.single,    posts.getById)
+     .post(app.custom.paths.posts.new,      posts.insert)
+     .put(app.custom.paths.posts.update,    posts.update)
+     .delete(app.custom.paths.posts.remove, posts.remove);
 }
