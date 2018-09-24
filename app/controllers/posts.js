e6b63f6a52454e9e9ae060fcb59054cc1e00077f
Klayton Faria <klayton.faria@farfetch.com>
2018-09-24 11:18:38 +0100

    refac: Update controller

--- app/controllers/posts.js
+++ app/controllers/posts.js
@@ -1,94 +1,41 @@
-module.exports = function(app) {
-  var model = require("../models/postsModel")(app),
-      slugfy = require("slug"),
-      slugs = require("../models/slugsModel")(app);
+const slugfy = require("slug");
+const Posts = require("../models/postsModel");
 
-  return {
-    name: "posts",
-
-    // Get all posts
-    getAll: function(req, res) {
-      model.select({}, function(err, data) {
-        if(err) {
-          app.custom.utils.responseJSON(500, res, err);
-        } else {
-          app.custom.utils.responseJSON(200, res, {posts : data});
+module.exports = [
+    {
+        method: 'GET',
+        path: '/api/v1/posts',
+        config: {
+            description: 'Get all the posts',
+            tags: ['api', 'v1', 'posts']
+        },
+        handler: () => {
+            return Posts.find();
         }
-      });
     },
+    {
+        method: 'POST',
+        path: '/api/v1/posts',
+        config: {
+            description: 'Create a new post',
+            tags: ['api', 'v1', 'posts']
+        },
+        handler: (req, reply) => {
+            const { title, content, excerpt, categories, tags } = req.payload;
+            const post = new Posts({
+                "type": "post",
+                "slug": slugfy(title),
+                "url": "/posts/" + slugfy(title),
+                title,
+                content,
+                excerpt,
+                date: new Date(),
+                modified: new Date(),
+                categories,
+                tags
+            });
 
-    // Get one post
-    getById: function(req, res) {
-      var filter;
-      if (req.params && req.params.id) {
-        filter = {_id:req.params.id};
-      }
-      model.select(filter, function(err, data) {
-        if(err) {
-          app.custom.utils.responseJSON(500, res, err);
-        } else {
-          app.custom.utils.responseJSON(200, res, {posts : data});
+            return post.save();
         }
-      });
-    },
-
-    // Remove post
-    remove: function(req, res) {
-      model.remove(req.params.id,
-        function(err, data) {
-          if(err) {
-            app.custom.utils.responseJSON(500, res, err);
-          } else {
-            app.custom.utils.responseJSON(200, res, {message: 'deleted'});
-          }
-        }
-      );
-    },
-
-    // Create post
-    insert: function(req, res) {
-      var doc = {
-            "type": "post",
-            "slug": slugfy(req.param("title")),
-            "url": "/posts/" + slugfy(req.param("title")),
-            "title": req.param("title") || "",
-            "content": req.param("content") || "",
-            "excerpt": req.param("excerpt") || "",
-            "date": new Date(),
-            "modified": new Date(),
-            "categories": ["category"] || [],
-            "tags": ["tag"] || []
-          };
-
-      model.insert(doc,
-        function(err, data) {
-          if(err) {
-            app.custom.utils.responseJSON(500, res, err);
-          } else {
-
-            slugs.insert(doc);
-
-            app.custom.utils.responseJSON(200, res, {message: 'created'});
-          }
-        }
-      );
-    },
-
-    // Update post
-    update: function(req, res) {
-      var id = req.params.id,
-          doc = {
-            "title": req.param('name'),
-            "modified": new Date()
-          };
-      model.update(id, doc, function(err, data) {
-        if(err) {
-          app.custom.utils.responseJSON(500, res, err);
-        } else {
-          app.custom.utils.responseJSON(200, res, {message: 'updated'});
-        }
-      });
     }
-
-  }
-};
+];
