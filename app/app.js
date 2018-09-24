4a6bf8855313554adcd44e8015dce112c75ab2f7
Klayton Faria <klayton.faria@farfetch.com>
2018-09-24 11:20:51 +0100

    refac: update app

--- app/app.js
+++ app/app.js
@@ -1,35 +1,79 @@
-// Module dependencies
-var express = require("express"),
-    PrettyError = require('pretty-error'),
-    app = express(),
-    fs = require('fs'),
-    http = require("http"),
-    path = require("path"),
-    utils = require("./extend/utils"),
-    paths = require("./config/paths").paths,
-    settings = require("./config/settings").constants;
+const hapi = require('hapi');
+const hapiSwagger = require('hapi-swagger');
+const { graphqlHapi, graphiqlHapi } = require('apollo-server-hapi');
+const mongoose = require('mongoose');
+const PrettyError = require('pretty-error');
+const Inert = require('inert');
+const Vision = require('vision');
+
+const Posts = require('./controllers/posts');
+const schema = require('./graphql/schemas/posts-schema');
+const pkg = require('./package');
+const settings = require("./config/settings").constants;
 
 // Pretty errors
-var pe = new PrettyError();
-pe.start();
+const prettyError = new PrettyError();
+prettyError.start();
 
-// Configurations
-app.configure(function() {
-  app.use(express.logger('dev'));
-  app.use(app.router);
-  app.set('port', settings.SERVICES_PORT);
 
-  // Customizing app
-  app.custom = app.custom || {};
-  app.custom.utils = utils;
-  app.custom.paths = paths;
-  app.custom.settings = settings;
+
+const init = async () => {
+  await server.register([
+    Inert,
+    Vision,
+    {
+      plugin: hapiSwagger,
+      options: {
+        info: {
+          title: 'Posts API Documentation',
+          version: pkg.version
+        },
+      }
+    }
+  ]);
+  await server.register({
+    plugin: graphiqlHapi,
+    options: {
+      path: '/graphiql',
+      graphiqlOptions: {
+        endpointURL: '/graphql'
+      },
+      route: {
+        cors: true
+      }
+    }
+  });
+  await server.register({
+    plugin: graphqlHapi,
+    options: {
+      path: '/graphql',
+      graphqlOptions: {
+        schema
+      },
+      route: {
+        cors: true
+      }
+    }
+  });
+  server.route(Posts);
+  await server.start();
+  console.log(`Server running at: ${server.info.uri}`);
+};
+
+
+const server = hapi.server({
+  port: settings.SERVICES_PORT,
+  host: 'localhost'
 });
 
-// Routes
-app.custom.utils.requireRecursive(app, "./routes/");
+mongoose.connect('mongodb://jones:007jones@ds111623.mlab.com:11623/cupcake', { useNewUrlParser: true });
+mongoose.connection.once('open', () => console.log('connected to database'));
 
-// Create and listen server application
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('\x1b[34mAPI server listening on port: ' + app.get('port'));
+process.on('unHandledRejection', (err) => {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
 });
+
+init();
